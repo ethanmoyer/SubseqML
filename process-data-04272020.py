f2828dd216e27d30f5d09f58f3c8237336756b03
ethan <57536747+ethanmoyer@users.noreply.github.com>
2020-05-19 21:46:03 -0400

    Added complete length 4 and length 5 data

--- process-data-04272020.py
+++ process-data-04272020.py
@@ -21,76 +21,22 @@
 # FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 # OTHER DEALINGS IN THE SOFTWARE.
 
-import tensorflow as tf
-# import theano
-import pandas as pd
-import numpy as np
-import matplotlib
-matplotlib.use('pdf')
-import matplotlib.pyplot as plt
-from tensorflow.keras.layers import Dense, Dropout, LSTM, Embedding, Activation, Lambda, Bidirectional, TimeDistributed, Flatten, Reshape
-from tensorflow.keras.preprocessing.sequence import pad_sequences
-from tensorflow.keras.preprocessing.text import one_hot
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.optimizers import Adam
-from sklearn.utils import class_weight
-import os
+'''' We are less concerened with this lower dimensional network because
+def create_lstm_test(input_dim, input_length, rnn_hidden_dim = RNN_HIDDEN_DIM, output_dim = OUTPUT_DIM, dropout = DROPOUT_RATIO):
+	model = Sequential()
 
-class Entry:
-	def __init__(self, gene, seq, seqs, contains, gap, op, b = 0):
-		self.gene = gene
-		self.seq = seq
-		self.seqs = seqs
-		self.contains = contains
-		self.gap = gap
-		self.b = b
-		self.op = op
+	model.add(Embedding(input_dim, OUTPUT_DIM, name='embedding_layer', input_length=input_length))
+	model.add(Bidirectional(LSTM(rnn_hidden_dim, return_sequences=True)))
+	model.add(Dropout(dropout))
+	model.add(Bidirectional(LSTM(rnn_hidden_dim)))
+	model.add(Dropout(dropout))
+	model.add(Dense(1, activation='sigmoid'))
+	model.compile('adam', 'binary_crossentropy', metrics=['accuracy'])
+	return model
+'''
 
-
-def create_entries():
-	mypath = 'data2/'
-	from os import listdir
-	from os.path import isfile, join
-	files = [f for f in listdir(mypath) if isfile(join(mypath, f))]
-	entries = []
-	for file in files[:20]:
-		split_file = file.split('_')
-		print(split_file)
-		with open(mypath + file, 'r') as f:
-			gene = split_file[0]
-			length = len(split_file)
-			if length == 6:
-				op = True
-				b = split_file[2]
-				gap = split_file[3]
-			elif length == 5:
-				op = False
-				gap = split_file[2]
-			else: 
-				continue
-			seq = split_file[4]
-			seqs = []
-			contains = []
-
-			for line in f:
-				line_seq = line.split(',')[0]
-				line_contains = line.split(',')[1]
-				if ('Subsequence' in line_seq):
-					continue
-				seqs.append(line_seq)
-				contains.append(int(line_contains.strip()))
-
-			f.close()
-			# for i in range(len(seqs)):
-			#	 print(seqs[i], contains[i])
-			if op:
-				entries.append(Entry(gene, seq, seqs, contains, gap, op, b))
-			else:
-				entries.append(Entry(gene, seq, seqs, contains, gap, op))
-	return entries
-
-def reverse_complement(seq):
-		return "".join("TGCA"["ACGT".index(s)] for s in seq[::-1])
+#def reverse_complement(seq):
+#		return "".join("TGCA"["ACGT".index(s)] for s in seq[::-1])
 
 	# dmd_sequence_r = reverse_complement(dmd_sequence)
 '''
@@ -129,26 +75,72 @@ def tf_dna_encode_embedding_table(dna_input, name="dna_encode"):
 	return encoded_dna
 '''
 
-EPCOHS = 20 #	an arbitrary cutoff, generally defined as "one pass over the entire dataset", used to separate training into distinct phases, which is useful for logging and periodic evaluation.
-BATCH_SIZE = 16 # a set of N samples. The samples in a batch are processed` independently, in parallel. If training, a batch results in only one update to the model.
+import tensorflow as tf
+from tensorflow import keras
+import pandas as pd
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+from tensorflow.keras.layers import Dense, Dropout, LSTM, Embedding, Activation, Lambda, Bidirectional, TimeDistributed, Flatten, Reshape
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.preprocessing.text import one_hot
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.optimizers import Adam
+from sklearn.utils import class_weight
+from sklearn.metrics import classification_report, confusion_matrix
+import os
+import tempfile
+
+class Entry:
+	def __init__(self, gene, seq, seqs, contains):
+		self.gene = gene
+		self.seq = seq
+		self.seqs = seqs
+		self.contains = contains
+
+
+def create_entries():
+	mypath = 'data2_1e4/'
+	from os import listdir
+	from os.path import isfile, join
+	files = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+	entries_4 = []
+	entries_5 = []
+	for file in files:
+		split_file = file.split('_')
+		print(split_file)
+		with open(mypath + file, 'r') as f:
+			length = len(split_file)
+			gene = split_file[0]
+			length = split_file[2]
+			seq = split_file[4]
+			seqs = []
+			contains = []
+			for line in f:
+				line_seq = line.split(',')[0]
+				line_contains = line.split(',')[1]
+				if ('Subsequence' in line_seq):
+					continue
+				seqs.append(line_seq)
+				contains.append(int(line_contains.strip()))
+			f.close()
+			if length == '4':
+				entries_4.append(Entry(gene, seq, seqs, contains))
+			elif length == '5':
+				entries_5.append(Entry(gene, seq, seqs, contains))
+	return entries_4, entries_5
+
+
+EPCOHS = 3 #	an arbitrary cutoff, generally defined as "one pass over the entire dataset", used to separate training into distinct phases, which is useful for logging and periodic evaluation.
+BATCH_SIZE = 32 # a set of N samples. The samples in a batch are processed` independently, in parallel. If training, a batch results in only one update to the model.
 OUTPUT_DIM = 64 # Embedding output
 
 DROPOUT_RATIO = 0.2 # proportion of neurones not used for training
 MAXSEQ = 5 # cuts text after number of these characters in pad_sequences
 RNN_HIDDEN_DIM = 32
 
-METRICS = [
-      tf.keras.metrics.TruePositives(name='tp'),
-      tf.keras.metrics.FalsePositives(name='fp'),
-      tf.keras.metrics.TrueNegatives(name='tn'),
-      tf.keras.metrics.FalseNegatives(name='fn'), 
-      tf.keras.metrics.BinaryAccuracy(name='accuracy'),
-      tf.keras.metrics.Precision(name='precision'),
-      tf.keras.metrics.Recall(name='recall'),
-      tf.keras.metrics.AUC(name='auc'),
-]
+def create_lstm(number_of_classes, time_steps, features, rnn_hidden_dim = RNN_HIDDEN_DIM, output_dim = OUTPUT_DIM, dropout = DROPOUT_RATIO, output_bias=None):
 
-def create_lstm(number_of_classes, time_steps, features, metrics = METRICS, rnn_hidden_dim = RNN_HIDDEN_DIM, output_dim = OUTPUT_DIM, dropout = DROPOUT_RATIO, output_bias=None):
 	model = Sequential()
 	if output_bias is not None:
 		output_bias = tf.keras.initializers.Constant(output_bias)
@@ -158,39 +150,8 @@ def create_lstm(number_of_classes, time_steps, features, metrics = METRICS, rnn_
 	model.add(TimeDistributed(LSTM(rnn_hidden_dim)))
 	model.add(Dropout(dropout))
 	model.add(Dense(1, activation='sigmoid', bias_initializer=output_bias))
-	model.compile('adam', 'mean_squared_error', metrics=['accuracy'])
+	model.compile('adam', loss='binary_crossentropy', metrics=['accuracy'])
 	return model
-'''' We are less concerened with this lower dimensional network because
-def create_lstm_test(input_dim, input_length, rnn_hidden_dim = RNN_HIDDEN_DIM, output_dim = OUTPUT_DIM, dropout = DROPOUT_RATIO):
-	model = Sequential()
-
-	model.add(Embedding(input_dim, OUTPUT_DIM, name='embedding_layer', input_length=input_length))
-	model.add(Bidirectional(LSTM(rnn_hidden_dim, return_sequences=True)))
-	model.add(Dropout(dropout))
-	model.add(Bidirectional(LSTM(rnn_hidden_dim)))
-	model.add(Dropout(dropout))
-	model.add(Dense(1, activation='sigmoid'))
-	model.compile('adam', 'binary_crossentropy', metrics=['accuracy'])
-	return model
-'''
-def create_plots(history):
-	plt.plot(history.history['acc'])
-	plt.plot(history.history['val_acc'])
-	plt.title('model accuracy')
-	plt.ylabel('accuracy')
-	plt.xlabel('epoch')
-	plt.legend(['train', 'test'], loc='upper left')
-	plt.savefig('accuracy.png')
-	plt.clf()
-
-	plt.plot(history.history['loss'])
-	plt.plot(history.history['val_loss'])
-	plt.title('model loss')
-	plt.ylabel('loss')
-	plt.xlabel('epoch')
-	plt.legend(['train', 'test'], loc='upper left')
-	plt.savefig('loss.png')
-	plt.clf()
 
 
 def letter_to_index(letter):
@@ -199,7 +160,7 @@ def letter_to_index(letter):
 
 
 # Connect this df with the create_entries function above
-def load_data(entries, test_split = 0.2, MAXSEQ = MAXSEQ):
+def load_data(entries_4, entries_5, test_split = 0.2, MAXSEQ = MAXSEQ):
 	matches = 0
 	total = 0
 	data = pd.DataFrame({
@@ -207,11 +168,11 @@ def load_data(entries, test_split = 0.2, MAXSEQ = MAXSEQ):
 		'contains': []
 	})
 	max_sentence = 0
-
+	entries = entries_4 + entries_5
 	for entry in entries:
 		if len(entry.seqs) > max_sentence:
 			max_sentence = len(entry.seqs)
-	print(max_sentence)
+	print('Number of timesteps per file: {}'.format(max_sentence))
 	contain_values = []
 	for entry in entries:
 		seq_df = pd.DataFrame({
@@ -232,21 +193,52 @@ def load_data(entries, test_split = 0.2, MAXSEQ = MAXSEQ):
 			})
 
 		seq_to_index_df['col'] = seq_df['col'].apply(lambda x: [int(letter_to_index(e)) for e in x])
-		''' Add back later when the fragment size increases. Simply removed for memory/speed purposes
+
 		padded_integer_df['col'] = [[0] * (MAXSEQ - len(x)) + x for x in seq_to_index_df['col']] + [[0] * MAXSEQ] * (max_sentence - len(seq_to_index_df['col']))
 
 		if len(entry.contains) < max_sentence:
 			entry.contains += [0] * (max_sentence - len(entry.contains))
-		'''
+
 		contain_values += entry.contains
 		# May have to change entry.contains to np.array(entry.contains)
 		data = data.append({'seqs': seq_to_index_df['col'].tolist(), 'contains': entry.contains}, ignore_index=True)
-		
+
 	#print(matches/total)
 	from sklearn.model_selection import train_test_split
-
+	'''
 	train_df, test_df = train_test_split(data, test_size=test_split)
 	train_df, val_df = train_test_split(train_df, test_size=test_split)
+	'''
+	data_labels = np.array(data['contains'].tolist())
+
+	test_size = int(len(entries_5) * (1 - test_split))
+	train_df = train_size[:len(entries_4)]
+	test_df = train_size[len(entries_4):]
+	test_df = test_df[:test_size]
+	val_df = test_df[test_size:]
+
+	pos = 0
+	neg = 0
+	for i in range(len(data_labels)):
+		for j in range(len(data_labels[0])):
+			if data_labels[i][j] == 1:
+				pos += 1
+			else:
+				neg += 1
+	total = pos + neg
+
+	print('Total data:\n    Total: {}\n    Positive: {} ({:.2f}% of total)\n'.format(total, pos, 100 * pos / total))
+
+	initial_bias = np.log([pos/neg])
+	print('Bias: {}'.format(initial_bias))
+
+	weight_for_0 = (1 / neg)*(total)/2.0 
+	weight_for_1 = (1 / pos)*(total)/2.0
+
+	class_weight = {0: weight_for_0, 1: weight_for_1}
+
+	print('Weight for class 0: {:.2f}'.format(weight_for_0))
+	print('Weight for class 1: {:.2f}'.format(weight_for_1))
 
 	train_labels = np.array(train_df['contains'].tolist())
 	val_labels = np.array(val_df.pop('contains').tolist())
@@ -266,34 +258,48 @@ def load_data(entries, test_split = 0.2, MAXSEQ = MAXSEQ):
 	print('Validation features shape:', val_features.shape)
 	print('Test features shape:', test_features.shape)
 	
-	return train_features, train_labels, test_features, test_labels, val_features, val_labels
+	return train_features, train_labels, test_features, test_labels, val_features, val_labels, initial_bias, class_weight
 
 
 if __name__ == "__main__":
 	print ('Gathering entries...')
-	entries = create_entries()
-	input_dim = len(entries) + 1
+	entries_4, entries_5 = create_entries()
+	input_dim = len(entries_4) + len(entries_5) + 1
 	print ('Loading data...')
-	train_features, train_labels, test_features, test_labels, val_features, val_labels = load_data(entries)	
+	train_features, train_labels, test_features, test_labels, val_features, val_labels, initial_bias, class_weight = load_data(entries_4, entries_5)	
 
 	print ('Creating model...')
-	model = create_lstm(input_dim, len(train_features[0]), len(train_features[0][0]))
+	model = create_lstm(input_dim, len(train_features[0]), len(train_features[0][0]), output_bias=initial_bias)
 	
 	model.summary()
-	
-	#prediction = model.predict(train_features, verbose = 1, batch_size = 1)
-	#results = model.evaluate(train_features, train_labels, batch_size=1, verbose=1)
-	#print("Loss: {:0.4f}".format(results[0]))
 
-	print ('Fitting model...')
+	print ('Training model...')
 
-	history = model.fit(train_features, train_labels, batch_size=4, epochs=EPCOHS, validation_data=(val_features, val_labels), verbose = 1)
-
-	# validate model on unseen data
-	score, acc = model.evaluate(train_features, train_labels, batch_size=4, verbose = 1)
-	print('Validation score:', score)
-	print('Validation accuracy:', acc)
-
+	initial_weights = os.path.join(tempfile.mkdtemp(),'initial_weights')
+	model.save_weights(initial_weights)
+	model.load_weights(initial_weights)
+	# May have to calculate class weights for each entry... 
+	history = model.fit(train_features, train_labels, batch_size=4, epochs=EPCOHS, validation_data=(val_features, val_labels), verbose = 1)#, class_weight=class_weight)
 	#create_plots(history)
 
-	quit()
+	test_predictions = model.predict_classes(test_features)
+
+	match = 0
+	total = 0
+
+	for i in range(len(test_predictions)):
+		for j in range(len(test_predictions[0])):
+			if test_predictions[i][j] == 1 and test_labels[i][j] == 1:
+				match += 1
+			if test_labels[i][j] == 1:
+				total += 1
+
+	quit(match/total)
+	baseline_results = model.evaluate(test_features, test_labels, batch_size=BATCH_SIZE, verbose=1)
+	for name, value in zip(model.metrics_names, baseline_results):
+ 		print(name, ': ', value)
+	# validate model on unseen data
+	score, acc = model.evaluate(train_features, train_labels, batch_size=4, verbose = 1)
+	
+	print('Validation score:', score)
+	print('Validation accuracy:', acc)

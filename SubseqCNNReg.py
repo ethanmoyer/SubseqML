7c268f2444d5359c7a1562c5bce6372453656ed8
ethan <57536747+ethanmoyer@users.noreply.github.com>
2020-07-17 21:23:30 -0400

    created processData function to generate kmer alignment tables entirely in python

--- SubseqCNNReg.py
+++ SubseqCNNReg.py
@@ -12,32 +12,32 @@ import matplotlib.pyplot as plt
 import math
 
 def letter_to_index(letter):
-	_alphabet = 'atgc'
+	_alphabet = 'ATCG'
 	return next(((i + 1) / 4 for i, _letter in enumerate(_alphabet) if _letter == letter), None)
 
-mypath = 'data/ref_sequences/'
+mypath = 'data/ref_sequences0/'
 
 data = []
 
+k = 50
+
 from os import listdir
 from os.path import isfile, join
-files = [f for f in listdir(mypath) if isfile(join(mypath, f)) and '20' in f]
+files = [f for f in listdir(mypath) if isfile(join(mypath, f)) and str(k) in f]
 
 for file in files:
 	data.append(pd.read_csv(mypath + file))
 
 for i, entry_data in enumerate(data):
 	#print(f'File: {files[i]}')
-	query = file.split('_')[1]
-	entry_data['Subsequence'] = entry_data['Subsequence'] + query
 
-	entry_data['Subsequence'] = entry_data['Subsequence'].apply(lambda x: [float(letter_to_index(elem)) for elem in x])
+	entry_data['kmer'] = entry_data['kmer'].apply(lambda x: [float(letter_to_index(elem)) for elem in x])
 
-	a = np.array(entry_data['Subsequence'].tolist())
+	a = np.array(entry_data['kmer'].tolist())
 
-	a = a.reshape((1, a.shape[0], 40))
+	a = a.reshape((1, a.shape[0], 50))
 
-	b = np.array(entry_data['Contains'].tolist())
+	b = np.array(entry_data['score'].tolist())
 
 	if 'features' not in locals():
 		features = a
@@ -70,7 +70,7 @@ if True:
 
 	model.add(Dropout(0.5))
 	model.add(Flatten())
-	model.add(Dense(981))
+	model.add(Dense(input_shape[1:2][0]))
 
 	# Compiles the model
 	model.compile(optimizer='adam', loss='mean_squared_error', metrics=['accuracy', 'mse'])
@@ -80,7 +80,7 @@ if True:
 if True:
 
 	# fit model
-	history = model.fit(X_train, y_train, epochs = 50, batch_size = 16, verbose=1, validation_data=(X_test, y_test))
+	history = model.fit(X_train, y_train, epochs = 100, batch_size = 16, verbose=1, validation_data=(X_test, y_test))
 
 	plt.plot(history.history['loss'])
 	plt.plot(history.history['val_loss'])
@@ -88,7 +88,7 @@ if True:
 	plt.ylabel('loss')
 	plt.xlabel('epoch')
 	plt.legend(['train', 'test'], loc='upper left')
-	plt.savefig('cnn_data/cnn0_20_abs_loss.png')
+	plt.savefig('cnn_data/cnn0_' + str(k) + '_abs_loss.png')
 	plt.clf()
 
 	a = [math.sqrt(e) for e in history.history['loss']]
@@ -100,5 +100,5 @@ if True:
 	plt.xlabel('epoch')
 	plt.legend(['train', 'test'], loc='upper left')
 
-	plt.savefig('cnn_data/cnn0_20_rel_loss.png')
+	plt.savefig('cnn_data/cnn0_' + str(k) + '_rel_loss.png')
 #print(yhat)
---
+++ processData.py
@@ -0,0 +1,64 @@
+from Bio import Align
+from Bio.Align import substitution_matrices
+
+from Bio import SeqIO
+
+from random import choice, random
+
+import pandas as pd
+
+# Parses through the records and returns the sequence
+def getFasta(fasta = "data/sequence.fasta"):
+	records = []
+	for record in SeqIO.parse(fasta, "fasta"):
+		 records.append(record)
+	return records
+
+
+# Create random subsequence with a given length
+# sequence = create_random_sequence(1000)
+def create_random_sequence(n):
+	dna = ["A","G","C","T"]
+	seq = ''
+	for i in range(n):
+	    seq += choice(dna)
+	return seq
+
+
+# Create a list of kmers given a sequence
+def create_kmer_list(seq, k):
+	return [seq[i:i + k] for i in range(len(seq) - k)]
+
+
+# Select a random kmer from a list of kmers
+def select_random_kmer(kmer_list):
+	return kmer_list[int(random() * len(kmer_list))]
+
+
+# Select random subsequence of length n from seq
+def select_random_subseq(seq, n):
+	i = int(random() * (len(seq) - n))
+	return seq[i: i + n]
+
+
+# Create table of alignment scores between subseq and every kmer in a list
+def create_alignment_table(subseq, kmer_list):
+	return [aligner.score(subseq, e) for e in kmer_list]
+
+
+parent_sequence = getFasta()
+k = 50
+
+for i in range(1000):
+	sequence = select_random_subseq(parent_sequence[0], 1000)
+	kmer_list = create_kmer_list(str(sequence.seq), k)
+	random_kmer = select_random_kmer(kmer_list)
+
+	aligner = Align.PairwiseAligner(match_score=1.0, mismatch_score = -2.0, gap_score = -2.5)
+	alignment_scores = create_alignment_table(random_kmer, kmer_list)
+	alignment_scores = [e / max(alignment_scores) for e in alignment_scores]
+
+	data = pd.DataFrame({'kmer': kmer_list, 'score': alignment_scores})
+	fdir = 'data/ref_sequences0/'
+	data.to_csv(fdir + random_kmer + '_' + str(k) + '.txt', mode = 'a', index = False)
+
